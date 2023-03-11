dd35ba29a28697773d533a43a2a005e3d7ac4343
Namrata Roy <royrio2000@gmail.com>
2023-03-11 20:28:27 +0530

    add components by Namrata

--- src/App.js
+++ src/App.js
@@ -1,22 +1,16 @@
 import './App.css';
+import Footer from './Footer';
+import Header from './Header';
+import Main from './Main';
+import Nav from './Nav';
 
 function App() {
   return (
   <>
-    <nav>
-      <ul style={list-style-type:'none'}>
-        <li><img src="" alt="" /></li>
-        <li><a href="/">Home</a></li>
-        <li><a href="/">About</a></li>
-        <li><a href="/">Menu</a></li>
-        <li><a href="/">Reservation</a></li>
-        <li><a href="/">Order Online</a></li>
-        <li><a href="/">Login</a></li>
-
-
-
-      </ul>
-    </nav>
+    <Nav/>
+    <Header/>
+    <Main/>
+    <Footer/>
   </>
   );
 }
---
+++ src/Nav.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Nav = () => {
+  return (
+    <>
+    <ul>
+        <li><img src="" alt="" /></li>
+        <li><a href="/">Home</a></li>
+        <li><a href="/">About</a></li>
+        <li><a href="/">Menu</a></li>
+        <li><a href="/">Reservation</a></li>
+        <li><a href="/">Order Online</a></li>
+        <li><a href="/">Login</a></li>
+    </ul>
+
+    </>
+  )
+}
+
+export default Nav
+
+
