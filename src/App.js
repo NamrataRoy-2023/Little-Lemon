5dcb0e8cac68c425361b636e098d0f0321a5acfa
Namrata Roy <royrio2000@gmail.com>
2023-03-11 20:13:20 +0530

    add metadata and nav items by Namrata

--- src/App.js
+++ src/App.js
@@ -2,9 +2,22 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      Homepage
-    </div>
+  <>
+    <nav>
+      <ul style={list-style-type:'none'}>
+        <li><img src="" alt="" /></li>
+        <li><a href="/">Home</a></li>
+        <li><a href="/">About</a></li>
+        <li><a href="/">Menu</a></li>
+        <li><a href="/">Reservation</a></li>
+        <li><a href="/">Order Online</a></li>
+        <li><a href="/">Login</a></li>
+
+
+
+      </ul>
+    </nav>
+  </>
   );
 }
 
