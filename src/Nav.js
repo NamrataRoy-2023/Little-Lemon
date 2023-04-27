9ca86b5a010acd87accaaf08044742895a8956d3
Namrata Roy <royrio2000@gmail.com>
2023-04-28 01:18:07 +0530

    images

--- src/Nav.js
+++ src/Nav.js
@@ -4,40 +4,42 @@ const Nav = (props) => {
   return (
     <>
     <nav className="navbar navbar-expand-lg bg-body-tertiary">
-  <div className="container-fluid">
-  <a class="navbar-brand" href="#">
-      <img src="/docs/5.3/assets/brand/bootstrap-logo.svg" alt="Logo" width="30" height="24" class="d-inline-block align-text-top"/>
-      Bootstrap
-    </a>
-    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-        <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="#">Home</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#">About</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#">Menu</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#">Reservation</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#">Order Online</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#">Login</a>
-        </li>
-      </ul>
+    <div className="container-fluid">
+    {/* <a className="navbar-brand" href="#">Navbar</a> */}
+      <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <a className="navbar-brand" href="#">
+        <img src="./logo.svg" alt="Logo" width="30" height="24" className="d-inline-block align-text-top"/>
+        Bootstrap
+      </a>
+      <div className="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+          <li className="nav-item">
+            <a className="nav-link active" aria-current="page" href="#">Home</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">About</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">Menu</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">Reservation</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">Order Online</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">Login</a>
+          </li>
+        </ul>
+      </div>
     </div>
-  </div>
-</nav>
+  </nav>
 
     </>
   )
 }
 
 export default Nav
-
-
