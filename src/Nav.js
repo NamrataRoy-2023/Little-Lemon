2123d4e7d41187d2218381c35cd46020eaf1e9b8
Namrata Roy <royrio2000@gmail.com>
2023-04-27 21:44:25 +0530

    Navbar added By Namrata

--- src/Nav.js
+++ src/Nav.js
@@ -1,17 +1,38 @@
 import React from 'react'
 
-const Nav = () => {
+const Nav = (props) => {
   return (
     <>
-    <ul>
-        <li><img src="" alt="" /></li>
-        <li><a href="/">Home</a></li>
-        <li><a href="/">About</a></li>
-        <li><a href="/">Menu</a></li>
-        <li><a href="/">Reservation</a></li>
-        <li><a href="/">Order Online</a></li>
-        <li><a href="/">Login</a></li>
-    </ul>
+    <nav className="navbar navbar-expand-lg bg-body-tertiary">
+  <div className="container-fluid">
+  <a class="navbar-brand" href="#">
+      <img src="/docs/5.3/assets/brand/bootstrap-logo.svg" alt="Logo" width="30" height="24" class="d-inline-block align-text-top"/>
+      Bootstrap
+    </a>
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">About</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Menu</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Reservation</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Order Online</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Login</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
 
     </>
   )
