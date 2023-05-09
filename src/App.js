1c0fd6f65463e2c86aa450b10ae5364ccab2a789
Namrata Roy <royrio2000@gmail.com>
2023-05-10 01:21:34 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,14 @@
-import './App.css';
-import Footer from './Footer';
-import Header from './Header';
-import Main from './Main';
-import Nav from './Nav';
-
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Routing from "./components/Routing";
+import Footer from "./components/Footer";
 function App() {
   return (
-  <>
-    <Nav/>
-    <Header/>
-    <Main/>
-    <Footer/>
-  </>
+    <>
+      <Navbar />
+      <Routing />
+      <Footer />
+    </>
   );
 }
 
